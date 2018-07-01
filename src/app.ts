bb5a4164e584eba85af8186063977ed931d711d5
Albert Parrón <al.parron@gmail.com>
2018-07-01 03:40:18 +0200

    added DTO

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
+import { PlayerDTO } from './models/playerDTO';
+
 const button = document.querySelector('button') as HTMLButtonElement;
 const input = document.querySelector('input') as HTMLInputElement;
 
 button.addEventListener(
   'click',
   () => {
-    if (!input.value.trim()) return;
-    console.log(input.value);
+    const playerName: string = input.value.trim();
+    if (!playerName) return;
+    const player: PlayerDTO = createPlayer(playerName);
+    console.log(player);
     input.value = '';
   },
   false
 );
+
+function createPlayer(name: string): PlayerDTO {
+  return { name, selected: false };
+}
---
+++ src/models/playerDTO.ts
@@ -0,0 +1,4 @@
+export interface PlayerDTO {
+  name: string;
+  selected: boolean;
+}
