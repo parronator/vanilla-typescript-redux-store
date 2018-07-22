1847a8b7ac0c96b0f5b19c90389a922d14c78b95
Albert Parrón <al.parron@gmail.com>
2018-07-22 19:07:25 +0200

    some corrections

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,33 @@
-import { PlayerDTO } from './models/playerDTO';
+import { IPlayer } from './models/player';
 import * as fromStore from './store';
-
-const reducers = {
-  players: fromStore.reducer
-};
-
-const store = new fromStore.Store(reducers);
 
 const button = document.querySelector('button') as HTMLButtonElement;
 const input = document.querySelector('input') as HTMLInputElement;
 const span = document.querySelector('span') as HTMLSpanElement;
 const playerList = document.querySelector('.players') as HTMLLIElement;
 
-button.addEventListener(
-  'click',
-  () => {
-    const playerName: string = input.value.trim();
-    if (!playerName) return;
-    const player: PlayerDTO = createPlayer(playerName);
-    store.dispatch({ type: 'ADD_PLAYER', payload: player });
-    input.value = '';
-  },
-  false
-);
+button.addEventListener('click', addPlayerAndReset, false);
 
-function createPlayer(name: string): PlayerDTO {
-  return { name, selected: false };
-}
+const reducers: { [key: string]: Function } = {
+  players: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
 
 store.subscribe(state => console.log('STATE =>', state));
 store.subscribe(state => {
   renderPlayers(state.players.data);
 });
 
-function renderPlayers(players: PlayerDTO[]) {
+function addPlayerAndReset() {
+  const playerName: string = input.value.trim();
+  if (!playerName) return;
+  const player: IPlayer = { name: playerName, selected: false };
+  store.dispatch({ type: 'ADD_PLAYER', payload: player });
+  input.value = '';
+}
+
+function renderPlayers(players: IPlayer[]) {
   span.innerHTML = players.length.toString();
   playerList.innerHTML = '';
 
