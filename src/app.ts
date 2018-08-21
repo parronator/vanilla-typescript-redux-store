48db9c7b28e2d61f50fa0503ba12e3a42ffb0357
Albert Parrón <al.parron@gmail.com>
2018-08-21 10:16:29 +0200

    modifications

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { IPlayer } from './models/Player/entity';
 import * as fromStore from './store';
+import { IAction } from './models/store';
 
 const button = document.querySelector('button') as HTMLButtonElement;
 const input = document.querySelector('input') as HTMLInputElement;
@@ -9,14 +10,15 @@ button.addEventListener('click', addPlayerAndReset, false);
 const store = new fromStore.Store(
   {},
   {
-    players: [{ name: 'Albert Parrón', selected: true }]
-  }
+    players: [{ name: 'Albert Parrón', selected: true }],
+  },
 );
 
 function addPlayerAndReset(): void {
   const name: string = input.value.trim();
   if (!name) return;
   const player: IPlayer = { name, selected: false };
-  store.dispatch({ type: 'ADD_PLAYER', payload: player });
+  const action: IAction = { type: 'ADD_PLAYER', payload: player };
+  store.dispatch(action);
   input.value = '';
 }
