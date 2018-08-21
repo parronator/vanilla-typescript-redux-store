fc8ae6d4ee696030a3082ef0a4cd8e6fb3031ac6
Albert Parrón <al.parron@gmail.com>
2018-08-21 10:03:35 +0200

    modifications

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
 import { IPlayer } from './models/Player/entity';
+import { IReducers } from './models/store';
 import * as fromStore from './store';
-
-const reducers = {
-  players: fromStore.reducer,
-};
-
-const store = new fromStore.Store(reducers);
 
 const button = document.querySelector('button') as HTMLButtonElement;
 const input = document.querySelector('input') as HTMLInputElement;
 
-button.addEventListener(
-  'click',
-  () => {
-    const name: string = input.value.trim();
-    if (!name) return;
-    const player: IPlayer = { name, selected: false };
-    store.dispatch({ type: 'ADD_PLAYER', payload: player });
-    input.value = '';
-  },
-  false,
-);
+button.addEventListener('click', addPlayerAndReset, false);
+
+function addPlayerAndReset(): void {
+  const name: string = input.value.trim();
+  if (!name) return;
+  const player: IPlayer = { name, selected: false };
+  store.dispatch({ type: 'ADD_PLAYER', payload: player });
+  console.log(store.value);
+  input.value = '';
+}
+
+const reducers: IReducers = {
+  players: fromStore.playersReducer,
+};
+
+const store = new fromStore.Store(reducers);
 
 store.subscribe(state => console.log('STATE =>', state));
